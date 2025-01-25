def7842a0db173b1b9c850b754cfd6d3f00d1ec5
nacycher <nacycher@163.com>
2025-01-25 22:51:09 +0800

    init project

---
+++ src/main/java/org/lb/studydocker/StudyDockerApplication.java
@@ -0,0 +1,13 @@
+package org.lb.studydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lb/studydocker/StudyDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lb.studydocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
